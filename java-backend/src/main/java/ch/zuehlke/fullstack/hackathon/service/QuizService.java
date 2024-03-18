7a356858e88f4299d4c3c94ef1f32c4d1ff058e1
Flavio Böni <flavio.boeni@zuehlke.com>
2024-03-18 17:31:11 +0100

    add h2-inmemory-db

--- java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/QuizService.java
+++ java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/QuizService.java
@@ -1,14 +1,25 @@
 package ch.zuehlke.fullstack.hackathon.service;
 
+import ch.zuehlke.qai.model.Quiz;
+import ch.zuehlke.qai.repository.QuizRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @Profile("!dev")
 @Service
 public class QuizService implements StartQuizSession {
+
+    private final QuizRepository quizRepository;
+
     public UUID startQuizSession(String topic) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        // api mit topic anfrage
+        Quiz quiz = new Quiz();
+        quiz.setTopic(topic);
+        Quiz savedQuiz = quizRepository.save(quiz);
+        return savedQuiz.getId();
     }
 }
